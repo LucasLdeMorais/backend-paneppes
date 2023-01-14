6d3da9b0716e4a189e146f5474cb6845ae932da2
Lucas L de Morais <lucasldemorais@gmail.com>
2023-01-14 15:43:22 -0300

    erro de digitação

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ app.get('/', (request, response) => {
 app.use(cors);
 app.use(express.json());
 app.use(routes);
-app.use('/.netlify/functions/api', router);
+app.use('/.netlify/functions/api', routes);
 
 module.exports.handler = serverless(app);
